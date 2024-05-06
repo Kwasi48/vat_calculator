d42e2b2ffea322a4779f5de0f11a83f6c4aca7b2
Kwasi48 <michael.abu@stu.ucc.edu.gh>
2024-05-06 15:08:17 +0000

    adding the logic files

---
+++ lib/model/classes/vat.dart
@@ -0,0 +1,20 @@
+
+class Vat {
+
+  Vat(this.nhil, this.getFund, this.covid, this.vat);
+  double nhil;
+  double getFund;
+  double covid;
+  double vat;
+
+  double inclusive (){
+    //TODO: function for calculating inclusive standard vat
+    return 0.0;
+  }
+
+  double exclusive (){
+    //TODO: function for calculating exclusive standard vat
+    return 0.0;
+  }
+
+}
