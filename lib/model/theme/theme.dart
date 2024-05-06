2dc2d8abbfd1a859b7d093fc79fcaed1ea33d34e
Kwasi48 <michael.abu@stu.ucc.edu.gh>
2024-05-06 12:59:16 +0000

    styling

--- lib/model/theme/theme.dart
+++ lib/model/theme/theme.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
- 
+
 
 
 
@@ -8,7 +8,9 @@ final  lightTheme = ThemeData(
       useMaterial3: true,
       brightness: Brightness.light,
   textTheme: TextTheme(
-    displayLarge: TextStyle( color: Colors.black, fontSize: 32, fontFamily: 'Abel' )
+    displayLarge: TextStyle( color: Colors.black, fontSize: 32, fontFamily: 'Abel', fontWeight: FontWeight.w700 ),
+    displayMedium: TextStyle(color: Colors.black, fontSize: 28, fontFamily: 'Abel', fontWeight: FontWeight.w600),
+    displaySmall: TextStyle(color: Colors.black, fontSize: 16, fontFamily: 'Abel', fontWeight: FontWeight.w500),
   )
 
   );
