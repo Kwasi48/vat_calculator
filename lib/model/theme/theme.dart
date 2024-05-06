6867dbc1629f1761b7d3f9fd02fb8073207d7c7e
Kwasi48 <michael.abu@stu.ucc.edu.gh>
2024-05-06 14:12:23 +0000

    home screen almost done

--- lib/model/theme/theme.dart
+++ lib/model/theme/theme.dart
@@ -4,14 +4,29 @@ import 'package:flutter/material.dart';
 
 
 final  lightTheme = ThemeData(
-      primaryColor: Colors.purple,
-      useMaterial3: true,
-      brightness: Brightness.light,
-  textTheme: TextTheme(
-    displayLarge: TextStyle( color: Colors.black, fontSize: 32, fontFamily: 'Abel', fontWeight: FontWeight.w700 ),
-    displayMedium: TextStyle(color: Colors.black, fontSize: 28, fontFamily: 'Abel', fontWeight: FontWeight.w600),
-    displaySmall: TextStyle(color: Colors.black, fontSize: 16, fontFamily: 'Abel', fontWeight: FontWeight.w500),
-  )
+      // primaryColor: Colors.purple,
+      // scaffoldBackgroundColor: const Co
+      // useMaterial3: true,
+  colorScheme: const ColorScheme(
+    brightness: Brightness.light,
+    primary: Color.fromRGBO(116, 105, 182, 1.0),
+    onPrimary: Colors.black,
+    secondary: Color.fromRGBO(173, 136, 198, 1.0),
+    onSecondary: Colors.black,
+    error: Colors.redAccent ,
+    onError: Colors.black,
+    background: Color.fromRGBO(255, 230, 230, 1.0),
+    onBackground:  Colors.black,
+    surface: Color.fromRGBO(255, 175, 209, 1.0),
+    onSurface: Colors.black,
+  ),
+     // brightness: Brightness.light,
+  textTheme: const TextTheme(
+    displayLarge: TextStyle( color: Colors.purple, fontSize: 32, fontFamily: 'Abel', fontWeight: FontWeight.w700 ),
+    displayMedium: TextStyle(color: Colors.purple, fontSize: 28, fontFamily: 'Abel', fontWeight: FontWeight.w600),
+    displaySmall: TextStyle(color: Colors.purple, fontSize: 16, fontFamily: 'Abel', fontWeight: FontWeight.w500),
+  ),
+
 
   );
 
--- lib/views/HomeScreen.dart
+++ lib/views/HomeScreen.dart
@@ -1,5 +1,6 @@
+//import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+//import 'package:google_fonts/google_fonts.dart';
 import 'package:vat_calculator/model/theme/theme.dart';
 
 
@@ -9,9 +10,40 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('App Under Construction 🚧', style: lightTheme.textTheme.displayLarge),
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('VAT CALCULATOR', style: lightTheme.textTheme.displayLarge,),
       ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              SizedBox(height: 200,),
+               SizedBox(
+                 height: 90, width: 200,
+                   child: standardRate('Standard Rate')),
+              SizedBox(height: 14,),
+              SizedBox(
+                  height: 90, width: 200,
+                  child: flatRate('Flat Rate')),
+            ],
+          ),
+        ),
+      )
+    );
+  }
+
+  Widget standardRate (String text){
+    return ElevatedButton(onPressed: (){},
+      child: Text(text),
+    );
+  }
+
+  Widget flatRate (String text){
+    return ElevatedButton(onPressed: (){},
+      child: Text(text),
     );
   }
 }
