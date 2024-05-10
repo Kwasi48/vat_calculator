a28af7586e10660ea702cc7cee6e0e277a586598
Kwasi48 <michael.abu@stu.ucc.edu.gh>
2024-05-10 11:48:44 +0000

    compute method done

--- lib/views/exclusive.dart
+++ lib/views/exclusive.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 
+import '../model/classes/inclucalc.dart';
+
 class exclusiveTax extends StatefulWidget {
    exclusiveTax({super.key});
 
@@ -53,13 +55,45 @@ class _exclusiveTaxState extends State<exclusiveTax> {
     }
                           }
      ,)
+
                       ],
                     ),
                   ),
-                )
+                ),
+                const SizedBox(height: 100,),
+                SizedBox(
+                  height: 200,
+                  width: 400,
+                  child: Card(
+                    child: Container(
+                      padding: EdgeInsets.all(60),
+                      child: Text(
+                        message!,
+                        style: TextStyle(fontSize: 30),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 70,
+                ),
+                calculateInclusive('Compute', context, userInput!),
+                SizedBox(height: 100,)
               ],
             ),
           )
         ));
+  }
+
+  Widget calculateInclusive(String text, BuildContext context, double input) {
+    return ElevatedButton(
+        onPressed: () {
+          //input = VatCal.gross;
+          VatInclusive inclusive = VatInclusive(input);
+          setState(() {
+            message = inclusive.inclusive(input).toString();
+          });
+        },
+        child: Text(text));
   }
 }
