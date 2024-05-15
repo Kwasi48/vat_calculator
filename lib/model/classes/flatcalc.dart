50bdf40b8ce816d3843a943d413e70e321e1b760
Kwasi48 <michael.abu@stu.ucc.edu.gh>
2024-05-15 11:14:01 +0000

    changes to the flatcalculation

--- lib/model/classes/flatcalc.dart
+++ lib/model/classes/flatcalc.dart
@@ -1,1 +1,17 @@
 //TODO: Add flatRate calculations
+
+import 'package:vat_calculator/model/classes/vat.dart';
+
+class Flatcalc extends Vat{
+
+  @override
+  double vat = 3/100;
+  @override
+  double covid = 1/100;
+
+  double flatVat (double gross){
+    double allFlat = vat + covid;
+    double totalFlat = allFlat * gross;
+    return totalFlat.roundToDouble();
+  }
+}
